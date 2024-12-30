bedc167ee7edf8b188a2e8aed8c5e92ec7956d6e
jacobhsu <jacob.hsu.tw@gmail.com>
2024-12-30 16:05:42 +0800

    feat: type productivity

---
+++ pages/data.js
@@ -0,0 +1,77 @@
+export const educationData = {
+  n: "Language",
+  d: "Education",
+  i: "https://www.apple.com/v/app-store/b/images/overview/icon_appstore__ev0z770zyxoy_large.png",
+  app1logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/b0/f1/a5/b0f1a533-5c9e-47c3-24b8-c110fae32bd9/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/230x0w.webp",
+  app1: "https://apps.apple.com/tw/app/多鄰國-極速學外語/id570060128",
+  app2logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/3f/53/4b/3f534b79-0734-90a0-08d4-9278726a2906/AppIconSpecial-0-0-1x_U007emarketing-0-10-0-85-220.png/246x0w.webp",
+  app2: "https://apps.apple.com/tw/app/英文多益知識王-單字-多益-托福-英檢-雅思必備吐司/id1611674456",
+  app3logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/47/0c/6e/470c6e73-9eb2-1882-aa5a-0de07e501632/AppIcon-0-0-1x_U007epad-0-85-220.png/246x0w.webp",
+  app3: "https://apps.apple.com/tw/app/聰明學習-word-up-單字救星-多益和學測刷題神器/id1447838299",
+  app4logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/13/34/a7/1334a7eb-9d8d-7c9c-3688-bad005a0d749/AppIcon-0-0-1x_U007emarketing-0-7-85-220.png/246x0w.webp",
+  app4: "https://apps.apple.com/tw/app/死神單字-7000單字-多益-托福-雅思-國中會考/id1570762449",
+  app5logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b4/c1/da/b4c1dabd-f0fd-8d9b-cc18-c5e881ebdf41/AppIcon-0-0-1x_U007ephone-0-1-0-85-220.png/246x0w.webp",
+  app5: "https://apps.apple.com/tw/app/刷刷庫-多益高分加速器/id6498889068",
+  app6logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/88/b5/c5/88b5c5f9-083c-a57f-8012-5b8d3b4b9706/AppIcon-0-0-1x_U007emarketing-0-10-0-0-85-220.png/246x0w.webp",
+  app6: "https://apps.apple.com/tw/app/todaii-english-學英文-更快-更好/id1625797002",
+  app7logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/61/37/6e/61376e69-f4f4-b988-4b7f-d068c4baa554/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/246x0w.webp",
+  app7: "https://apps.apple.com/tw/app/wordbranch-字首字根字尾字典/id1080556151?platform=iphone",
+  app8logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ac/c5/28/acc528d1-413a-d696-887c-994ca3669d2c/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/246x0w.webp",
+  app8: "https://apps.apple.com/tw/app/ted/id376183339",
+  app9logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/87/ed/d9/87edd95f-141c-ff5e-635e-602bcdbe083a/AppIcon-1x_U007emarketing-0-6-0-0-85-220-0.png/246x0w.webp",
+  app9: "https://apps.apple.com/tw/app/一点英语-刷视频也能学英语/id1392803612?platform=iphone",
+  app10logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ba/2c/8b/ba2c8b4d-d874-ce0e-8c37-b7c70e1f2013/AppIconInternational-0-0-1x_U007emarketing-0-10-0-0-85-220.png/246x0w.webp",
+  app10:
+    "https://apps.apple.com/tw/app/drops點滴學語言-學習韓語葡萄牙語日語俄語和其他的外語/id939540371",
+  app11logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/2d/3c/60/2d3c60b6-e52e-1e02-b809-5cec23941984/AppIcon-0-0-1x_U007emarketing-0-7-0-85-220.png/246x0w.webp",
+  app11:
+    "https://apps.apple.com/tw/app/boldvoice-accent-training/id1567841142?platform=iphone",
+  app12logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/8b/6d/b8/8b6db8c7-441c-eaeb-b6c6-6533fa172f2e/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/246x0w.webp",
+  app12:
+    "https://apps.apple.com/tw/app/letmespeak-英文學習遊戲輕鬆背單字/id1478889848?",
+  ls: [
+    {
+      l: "Voicetube",
+      i: "https://vt-cdn.voicetube.com/assets/img/logo/favicon/16.png",
+      u: "https://tw.voicetube.com",
+    },
+    {
+      l: "Lingvist",
+      i: "https://lingvist.com/favicon.ico",
+      u: "https://learn.lingvist.com",
+    },
+    {
+      l: "React JS course",
+      i: "",
+      u: "https://reactjs.org/",
+    },
+  ],
+};
+
+export const productivityData = {
+  n: "Habit",
+  d: "Productivity",
+  i: "https://www.apple.com/v/app-store/b/images/overview/icon_appstore__ev0z770zyxoy_large.png",
+  app1logo:
+    "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/2d/b4/4e/2db44e90-1f03-d8bf-916c-e082b4c575d3/AppIcon-0-0-1x_U007epad-0-0-0-85-220.png/246x0w.webp",
+  app1: "https://apps.apple.com/tw/app/forest-專注森林-讀書專注番茄鐘/id866450515",
+  ls: [
+    {
+      l: "",
+      i: "",
+      u: "",
+    },
+  ],
+};
